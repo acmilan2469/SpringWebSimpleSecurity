b90adc0166d03727978b8e1676a806b11e0b0ade
Milan Nirola <acmilan2469@gmail.com>
2021-10-20 14:58:16 +0545

    creating project from spring.io

---
+++ src/main/java/mn/milan/SimpleSpringSecurityImplementationApplication.java
@@ -0,0 +1,13 @@
+package mn.milan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringSecurityImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringSecurityImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mn/milan/SimpleSpringSecurityImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package mn.milan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringSecurityImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
